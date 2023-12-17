3a951416e1f7be7abfb14ded20ab63d3b5a8c47a
danila <dw@mail.ru>
2023-12-17 23:11:04 +0300

    update(users): fix async code, add create task, add gather

---
+++ src/modules/users/schemas/__init__.py
@@ -0,0 +1,8 @@
+from src.modules.users.schemas.schemas import (
+    CreateUserSchema,
+    FullUserSchema,
+    GetUserFiltersSchema,
+    LoginUserSchema,
+    UpdateUserSchema,
+    UsersWithPaginationSchema,
+)
