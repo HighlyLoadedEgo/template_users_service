46e1a8fe2656f3f1ac9b49780b8128a2823b91f0
danila <dw@mail.ru>
2024-01-13 17:56:30 +0300

    feat(src): add response models to endpoints, delete middleware for secure doc, add exception handler, fix code in repository, delete endpoint for search by email and delete use case with this logic and reader method

--- src/application/api/swagger.py
+++ src/application/api/swagger.py
@@ -1,6 +1,9 @@
 from typing import Any
 
-from fastapi import FastAPI
+from fastapi import (
+    Depends,
+    FastAPI,
+)
 from fastapi.openapi.docs import (
     get_redoc_html,
     get_swagger_ui_html,
@@ -9,14 +12,16 @@ from fastapi.openapi.utils import get_openapi
 from fastapi.responses import HTMLResponse
 
 from src.application.api.config import AppConfig
+from src.application.api.security.permission_check import check_doc_permission
 
 
 def init_swagger_endpoints(app: FastAPI, app_config: AppConfig) -> None:
     """Initialize swagger endpoints with config."""
+    permission_depends = check_doc_permission(app_config=app_config)
 
     async def get_swagger_documentation() -> HTMLResponse:
         return get_swagger_ui_html(
-            openapi_url=app_config.openapi_url, title=app_config.doc_title
+            openapi_url=app_config.openapi_url, title=app_config.title
         )
 
     async def openapi() -> dict[str, Any]:
@@ -26,21 +31,24 @@ def init_swagger_endpoints(app: FastAPI, app_config: AppConfig) -> None:
 
     async def get_redoc() -> HTMLResponse:
         return get_redoc_html(
-            openapi_url=app_config.openapi_url, title=app_config.doc_title
+            openapi_url=app_config.openapi_url, title=app_config.title
         )
 
     app.get(
         app_config.doc_url,
         tags=["documentation"],
         include_in_schema=False,
+        dependencies=[Depends(permission_depends)],
     )(get_swagger_documentation)
     app.get(
         app_config.openapi_url,
         tags=["documentation"],
         include_in_schema=False,
+        dependencies=[Depends(permission_depends)],
     )(openapi)
     app.get(
         app_config.redoc_url,
         tags=["documentation"],
         include_in_schema=False,
+        dependencies=[Depends(permission_depends)],
     )(get_redoc)
--- src/core/common/exceptions.py
+++ src/core/common/exceptions.py
@@ -1,11 +1,8 @@
 from dataclasses import dataclass
-from typing import Any
 
 
 @dataclass(eq=False)
 class BaseAppException(Exception):
-    data: Any = None
-
     @property
     def message(self) -> str:
         """Method for error message."""
